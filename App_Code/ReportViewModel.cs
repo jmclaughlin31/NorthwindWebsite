788eb8cf26fa6ece1f96f935bd6b3cddbf4da84b
JODY MC LAUGHLIN <jmclaughlin31@qub.ac.uk>
2017-02-03 15:27:11 +0000

    Commit all

---
+++ App_Code/ReportViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.PowerBI.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.PowerBI.Api.V1.Models;
+
+/// <summary>
+/// Summary description for ReportViewModel
+/// </summary>
+//public class ReportViewModel
+//{
+//    public ReportViewModel()
+//    {
+//        //
+//        // TODO: Add constructor logic here
+//        //
+//    }
+//}
+
+namespace PowerBI.Models
+{
+    public class ReportViewModel
+    {
+        public Report Report { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
---
+++ App_Code/ReportsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.PowerBI.Api.V1.Models;
+
+/// <summary>
+/// Summary description for ReportsViewModel
+/// </summary>
+//public class ReportsViewModel
+//{
+//	public ReportsViewModel()
+//	{
+//		//
+//		// TODO: Add constructor logic here
+//		//
+//	}
+//}
+
+namespace PowerBI.Models
+{
+    public class ReportsViewModel
+    {
+        public List<Report> Reports { get; set; }
+    }
+}
